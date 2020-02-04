9781bae581fb56fd3827e73343dd57d4e40499b5
coffeemori92 <coffeemori92@naver.com>
2020-02-04 15:03:28 +0900

    puppeteer, csv-stringify로 csv파일 로컬 저장 까지

---
+++ 03_csv-puppeteer/index.js
@@ -0,0 +1,48 @@
+import parse from 'csv-parse/lib/sync';
+import csvStringify from 'csv-stringify/lib/sync';
+import fs from 'fs';
+import path from 'path';
+import puppeteer from 'puppeteer';
+
+const csv = fs.readFileSync(path.join(__dirname, 'csv/data.csv'));
+const records = parse(csv.toString('utf-8'));
+
+const crawler = async () => {
+    const result = [];
+    const browser = await puppeteer.launch({headless: process.env.NODE_ENV === 'production'});
+    await Promise.all(records.map(async (e, i) => {
+        const page = await browser.newPage();
+        await page.goto(e[1]);
+        const scoreEl = await page.$('.score.score_left .star_score');
+        if(scoreEl){
+            const text = await page.evaluate(tag => tag.textContent.trim(), scoreEl);
+            console.log(e[0], '평점', text);
+            result[i] = [e[0], e[1], text];
+        }
+        await page.close();
+    }));
+    // const [page, page2, page3] = await Promise.all([
+    //     browser.newPage(),
+    //     browser.newPage(),
+    //     browser.newPage()
+    // ]);
+    // await Promise.all([
+    //     page.goto('https://google.com'),
+    //     page2.goto('https://naver.com'),
+    //     page3.goto('https://daum.net')
+    // ]);
+    // console.log('working');
+    // await Promise.all([
+    //    page.waitFor(3000),
+    //    page2.waitFor(1000),
+    //    page3.waitFor(2000) 
+    // ]);
+    // await page.close();
+    // await page2.close();
+    // await page3.close();
+    await browser.close();
+    const str = csvStringify(result);
+    fs.writeFileSync(path.join(__dirname, 'csv/result.csv'), str);
+};
+
+crawler();
--- 05_axios-cheerio/index.js
+++ 05_axios-cheerio/index.js
@@ -2,6 +2,7 @@ import xlsx from 'xlsx';
 import path from 'path';
 import axios from 'axios';
 import cheerio from 'cheerio';
+import add_to_sheet from './add_to_sheet';
 
 const workbook = xlsx.readFile(path.join(__dirname, 'xlsx/data.xlsx'));
 // console.log(Object.keys(workbook.Sheets));
@@ -11,7 +12,7 @@ const records = xlsx.utils.sheet_to_json(ws);
 // console.log(records);
 
 for (const [i, e] of records.entries()){
-//c    console.log(i, e);
+//    console.log(i, e);
 }
 
 const crawler = async () => {
@@ -28,6 +29,7 @@ const crawler = async () => {
 };
 
 const crawler2 = async () => {
+    add_to_sheet(ws, 'C1', 's', '평점');
     for(const [i, e] of records.entries()){
         const response = await axios.get(e.링크);
         if(response.status === 200){
@@ -35,8 +37,11 @@ const crawler2 = async () => {
             const $ = cheerio.load(html);
             const text = $('.score.score_left .star_score').text().trim();
             console.log(e.제목, '평점', text);
+            const newCell = `C${i + 2}`;
+            add_to_sheet(ws, newCell, 'n', parseFloat(text));
         }
     }
+    xlsx.writeFile(workbook, path.join(__dirname, 'xlsx/result.xlsx'));
 };
 
 crawler2();

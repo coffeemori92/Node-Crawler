dfb4375abdd961aaac5f6c2abf9729cb9d6543cd
coffeemori92 <coffeemori92@naver.com>
2020-02-06 21:02:43 +0900

    facebook 조정

---
+++ 08_facebook-login-logout/index.js
@@ -0,0 +1,46 @@
+import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const crawler = async () => {
+    try{
+        const browser = await puppeteer.launch({
+            headless: false,
+            // args: ['--window-size=1536, 864']
+            args: ['--disable-notifications']
+        });
+        const page = await browser.newPage();
+        // await page.setViewport({
+        //     width: 1536,
+        //     height: 864
+        // });
+        await page.goto('https://www.facebook.com/');
+        // const id = process.env.EMAIL;
+        // const password = process.env.PASSWORD;
+        // await page.evaluate((id , password) => {
+        //     document.querySelector('#email').value = id;
+        //     document.querySelector('#pass').value = password;
+        //     document.querySelector('#loginbutton').click(); 
+        // }, id, password);
+        await page.type('#email', process.env.EMAIL);
+        await page.type('#pass', process.env.PASSWORD);
+        await page.hover('#loginbutton');
+        await page.waitFor(3000);
+        await page.click('#loginbutton');
+        await page.waitForResponse(response => {
+            console.log(response.url());
+            return response.url().includes('login_attempt');
+        });
+        await page.waitFor(3000);
+        await page.click('#userNavigationLabel');
+        await page.waitFor(10000);
+        await page.click('li.navSubmenu:last-child');
+        // await page.close();
+        // await browser.close();
+    }catch(error){
+        console.log(error);
+    }
+};
+
+crawler();
